4546e77635539bba51ddc744c3fa9e7844fab2e3
smael <mael.sourisseau@ynov.com>
2024-12-16 09:19:46 +0100

    Started world5

---
+++ bash_scripts/world5.sh
@@ -0,0 +1,22 @@
+cat <<EOF > /home/world5/.hnt
+/
+Un récit se forma dans l'ombre.
+Sur les collines, le vent soufflait fort.
+Rien ne semblait pouvoir les arrêter.
+/
+Soudain, un cri brisa le silence.
+Habitués à l'obscurité, ils avancèrent.
+Aucun d’eux ne se retourna.
+Rares étaient ceux qui osaient suivre cette route.
+Enfin, ils atteignirent leur destination.
+/
+Rien ne les préparait à ce qu’ils allaient trouver.
+On entendit un bruit étrange derrière eux.
+Ce n’était qu’un début, pensèrent-ils.
+Kilomètres après kilomètres, ils restaient concentrés.
+Yeux rivés sur l’horizon, ils avançaient.
+Ombres et lumières jouaient sur les murs.
+Une aventure unique venait de commencer.
+
+Line 91.
+EOF
